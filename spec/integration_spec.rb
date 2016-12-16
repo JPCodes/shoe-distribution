a7dfaa8a5f436af90b5f61f51528f5d9975e01cb
Jim Padilla <jp_code2013@yahoo.com>
2016-12-16 14:19:23 -0800

    Add integ spec for add/view store

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -16,3 +16,13 @@ describe('Views /stores', {:type => :feature}) do
     expect(page).to have_content('Shoe central')
   end
 end
+
+describe('Add a new store and see the store information', {:type => :feature}) do
+  it('allows a user to click a store to see its details') do
+    visit('/stores')
+    fill_in('name_input', :with => 'Nike outlet')
+    click_button('Add store')
+    click_link('Nike outlet')
+    expect(page).to have_content('Manage Store: Nike outlet')
+  end
+end
