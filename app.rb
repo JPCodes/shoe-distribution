00ae6fba91a8a46d6100fee4cb93dfebe75b3504
Jim Padilla <jp_code2013@yahoo.com>
2016-12-16 09:54:37 -0800

    Add new store entry form on stores view

--- app.rb
+++ app.rb
@@ -2,3 +2,18 @@ require("bundler/setup")
 require("pry")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/stores') do
+  @all_stores = Store.all()
+  erb(:stores)
+end
+
+post('/stores/create_new_store') do
+  name_input = params[:name_input]
+  new_store = Store.create({:name => name_input})
+  redirect('/stores')
+end
